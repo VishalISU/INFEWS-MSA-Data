31dc2336deec8d4a83137e296c9c774b7a921456
vwinbglab <vishalm@iastate.edu>
2023-05-30 17:13:30 -0500

    added future scenario to swat

--- pages/2_SWAT.py
+++ pages/2_SWAT.py
@@ -62,9 +62,45 @@ fig.update_layout(
 st.plotly_chart(fig)
 
 
-if st.checkbox('Show historical dataset'):
+st.subheader('Future Scenario')
+
+# Create the DataFrame
+df2 = pd.DataFrame(chart_data_fut)
+# Transpose for better plotting
+df2=df2.transpose()
+
+Choice3 = st.selectbox(
+    'Select Crop 1:',
+     df2.columns)
+Choice4 = st.selectbox(
+    'Select Crop 2:',
+     df2.columns)
+# Create the bar graph
+fig1 = go.Figure()
+
+fig1.add_trace(go.Bar(x=df2.index, y=df2[Choice3], name=Choice3 ))
+fig1.add_trace(go.Bar(x=df2.index, y=df2[Choice4],name=Choice4))
+
+# Iterate over the columns starting from the second column
+#for i in range(1, len(df.columns)):
+#    fig.add_trace(go.Bar(x=df.index, y=df.iloc[:, i], name=df.columns[i]))
+
+# Set the layout
+fig1.update_layout(
+    title='Crops Yields by year',
+    xaxis_title='Year',
+    yaxis_title='Yields (kg/ha)',
+    xaxis=dict(
+        tickmode='linear',
+        dtick = 1
+    )
+)    
+
+st.plotly_chart(fig1)
+
+if st.checkbox('Show current baseline dataset'):
     chart_data_hist
 
-if st.checkbox('Show future dataset'):
+if st.checkbox('Show future scenario dataset'):
     chart_data_fut 
 
