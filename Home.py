880d96d9ee9c96cff8c0b6a9a3981d5245d26b8c
Muralidharan <vishalm@iastate.edu>
2024-05-16 11:07:47 -0500

    Added Flow diagram to Home page, minor placement mods on Home

--- Home.py
+++ Home.py
@@ -10,9 +10,13 @@ import plotly.graph_objects as go
 st.write('# :city_sunrise: Iowa Urban FEWS Dashboard :deciduous_tree:')  #st.title('Avocado Prices dashboard')
 
 st.markdown(
-    '''    This is an app framework built specifically for the IOWA Urban FEWS projects.
+    '''     **👈 Select a page from the sidebar** to get started exploring the data  '''
+)
+# Include the image MSAInFlow.png
+st.image('MSAInFlow.png')
 
-    **👈 Select a page from the sidebar** to get started exploring the data 
+st.markdown(
+    '''    
     ### Want to learn more?
     - Check out [Iowa Urban FEWS website](https://iowa-urbanfews.cber.iastate.edu/)
     - :speech_balloon: Have a suggestion? Please leave your comments in the [feedback survey](https://iastate.qualtrics.com/jfe/form/SV_5BVuGwhpC1s1RR4) 
@@ -20,5 +24,4 @@ st.markdown(
     '<a href="mailto:infews@iastate.edu">infews@iastate.edu!</a>', unsafe_allow_html=True
 
 )
-
 
